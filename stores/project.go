e469844cb85d265f097c36b4d95d98b9ee17b3d9
Xiao Rong Tao <xiaorongtao@le.com>
2016-11-18 17:54:31 +0800

    tmp Signed-off-by: Xiao Rong Tao <xiaorongtao@le.com>

--- stores/project.go
+++ stores/project.go
@@ -8,8 +8,37 @@ type ProjectStore struct {
 	*Store
 }
 
-func (p *ProjectStore) FindWith(tagId int64, key string, offset, limit int) ([]*models.Project, error) {
-	p.Master.Model(&projects).Where(where, ...)
+func (p *ProjectStore) FindWith(querySQL string, offset, limit int) (models.ProjectList, error) {
+	var projects models.ProjectList
+	err := p.Master.Model(&models.Project{}).Column("Id", "Name", "Size", "Version", "LogoURL", "Dlink", "Tags").Where(querySQL).Select(&projects)
+
+	return projects, err
+}
+
+// func (p *ProjectStore) FindWith(querySQL string, offset, limit int) (models.ProjectList, error) {
+// 	var projects models.ProjectList
+// 	_, err := p.Master.Query(&projects, "SELECT projects.id, projects.name, projects.version, projects.size, projects.logo_url, projects.dlink FROM projects WHERE "+querySQL)
+
+// 	idsStr := ""
+// 	for _, p := range projects {
+// 		idsStr += (strconv.FormatInt(p.Id, 10) + ",")
+// 	}
+// 	var ptags map[string]string
+// 	p.Master.Query(&ptags, "SELECT pt.project_id, t.name FROM tags t JOIN project_tags pt ON t.id = pt.tag_id WHERE pt.project_id in (?)", idsStr[:len(idsStr)-1])
+// 	fmt.Printf("ptags: %v", ptags)
+// 	return projects, err
+// }
+
+func (p *ProjectStore) FindWithTag(tagId int64, offset, limit int) ([]*models.Project, error) {
+	var projects []*models.Project
+	err := p.Master.Model(&projects).Column("project.*", "TagArray").Where("tag_id = ?", tagId).Select()
+	return projects, err
+}
+
+func (p *ProjectStore) FindWithKey(key string, offset, limit int) ([]*models.Project, error) {
+	var projects []*models.Project
+	err := p.Master.Model(&projects).Column("project.*", "TagArray").Where("name like ? or description like ?", key).Select()
+	return projects, err
 }
 
 func (u *ProjectStore) FindAll(offset, limit int) ([]*models.Project, error) {
--- stores/stores.go
+++ stores/stores.go
@@ -3,6 +3,8 @@ package stores
 import (
 	// "fmt"
 	"crypto/tls"
+	log2 "log"
+	"os"
 
 	log "github.com/Sirupsen/logrus"
 	"gopkg.in/pg.v5"
@@ -50,6 +52,8 @@ func setupDB(config map[string]interface{}) *pg.DB {
 		// Whether to use secure TCP/IP connections (TLS).
 
 	})
+	pg.SetQueryLogger(log2.New(os.Stdout, "postgres", 1))
+	pg.SetLogger(log2.New(os.Stdout, "postgres", 1))
 	log.WithField("server", "starting").Info("init db success...")
 	log.Debug(db)
 	return db
