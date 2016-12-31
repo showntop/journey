7e5ddaf08dddcbad2e97e422ba140dd82cb5712e
showntop <showntop@163.com>
2017-01-01 02:40:58 +0800

    [db]
    Signed-off-by: showntop <showntop@163.com>

--- db/migrations/20161128165317_modify_project_asset_to_array.sql
+++
@@ -1,8 +0,0 @@
-
--- +goose Up
--- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE projects ADD COLUMN assets text[] DEFAULT '{}';
-
--- +goose Down
--- SQL section 'Down' is executed when this migration is rolled back
-ALTER TABLE projects DROP COLUMN assets RESTRICT;
