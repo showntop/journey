86e726b66550215bc6cf70ea9388a128330e854b
showntop <showntop@163.com>
2017-01-02 14:03:32 +0800

    [deploy]
    Signed-off-by: showntop <showntop@163.com>

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,22 @@ cd $GOPATH/src/github.com/showntop/journey
 
 mkdir -p /home/wimill/runtime
 mkdir -p /home/wimill/runtime/logs
-
 cp ./supervisord.conf /home/wimill/runtime
+echo 'created runtime、logs、supervisord conf'
 
 godep go build 
 go install
+echo 'installed journey'
 
 /bin/ps auxf | /bin/grep "journey" | /bin/grep -v grep | /usr/bin/awk '{print $2}' | /usr/bin/xargs -n1 /bin/kill -2
 
-nohup supervisord -c /home/wimill/runtime/supervisord.conf > /home/wimill/runtime/supervisord.log &
+echo 'killed journey'
+
+if [ ! -f "/home/wimill/runtime/supervisor.sock" ]; then
+	#statements
+	supervisord -c /home/wimill/runtime/supervisord.conf > /home/wimill/runtime/supervisord.log &
+	echo 'sstarted upervisord'
+fi
+
+###检查启动chengg
+echo 'done'
