3b90411b6e43a95bd9df58501f694568e15315fa
showntop <showntop@163.com>
2017-01-02 17:58:18 +0800

    [category]
    Signed-off-by: showntop <showntop@163.com>

--- stores/project.go
+++ stores/project.go
@@ -31,17 +31,9 @@ func (p *ProjectStore) FindAll(offset, limit int) ([]*models.Project, error) {
 	return projects, err
 }
 
-func (u *ProjectStore) FindAllByCategory(cid int64, offset, limit int) ([]*models.Project, error) {
+func (p *ProjectStore) FindAllByCategory(cid int64, offset, limit int) ([]*models.Project, error) {
 	projects := []*models.Project{}
-	err := u.Master.Model(&projects).Related(&[]*models.Tag{}, "tags").Where("category_id = ?", cid).Offset(offset).Limit(limit).Error
-
-	///pos
-	// for _, p := range projects {
-	// 	for _, tag := range p.TagArray {
-	// 		p.Tags = append(p.Tags, tag.Name)
-	// 	}
-	// }
-	//pos
+	err := p.Master.Preload("Tags").Where("category_id = ?", cid).Select("projects.id, projects.name, projects.version, projects.size, projects.logo_url, projects.dlink").Offset(offset).Limit(limit).Find(&projects).Error
 
 	return projects, err
 }
