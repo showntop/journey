3891560bcec3b2bcb05725cfa6f0592febfb1e18
showntop <showntop@163.com>
2017-03-18 15:43:06 +0800

    [project]
    Signed-off-by: showntop <showntop@163.com>

--- stores/project.go
+++ stores/project.go
@@ -49,6 +49,7 @@ func (p *ProjectStore) FindAllByCategory(cid int64, offset, limit int) ([]*model
 func (u *ProjectStore) Find(id int64) (*models.Project, error) {
 	project := &models.Project{}
 	err := u.Master.First(project, id).Error
+	project.LogoURL = QINIU_URL + project.LogoURL
 	for i := len(project.Assets) - 1; i >= 0; i-- {
 		project.Assets[i] = QINIU_URL + project.Assets[i]
 	}
