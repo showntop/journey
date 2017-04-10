2ba6fd0f7ed033f435fb1ad7aa8bdcd584afc132
Xiao Rong Tao <xiaorongtao@le.com>
2017-04-10 18:00:32 +0800

    [assets] banner
    Signed-off-by: Xiao Rong Tao <xiaorongtao@le.com>

--- stores/asset.go
+++ stores/asset.go
@@ -13,32 +13,33 @@ type AssetStore struct {
 var (
 	bootAssets = [...]*models.Asset{
 		&models.Asset{
-			URL:    "http://oizfueie4.bkt.clouddn.com/assets/boots/splashPage.png",
+			URL:    "http://ohan0t6mr.bkt.clouddn.com/bar/jpg/huochairen.jpg",
 			Action: "com.hotkuy.app.android.activity.IntroduceActivity",
-			Params: "app_id:10001",
+			Params: "app_id:1",
 		},
 	}
 
 	bannerAssets = [...]*models.Asset{
 		&models.Asset{
-			URL:    "http://oizfueie4.bkt.clouddn.com/assets/banners/banner_xiaoxiaole.jpg",
+			URL:    "http://ohan0t6mr.bkt.clouddn.com/bar/jpg/huochairen.jpg",
 			Action: "com.hotkuy.app.android.activity.IntroduceActivity",
-			Params: "app_id:10002",
+			Params: "app_id:1",
+		},
+
+		&models.Asset{
+			URL:    "http://ohan0t6mr.bkt.clouddn.com/bar/jpg/wangzherongyao.jpg",
+			Action: "com.hotkuy.app.android.activity.IntroduceActivity",
+			Params: "app_id:200002",
 		},
 		&models.Asset{
-			URL:    "http://oizfueie4.bkt.clouddn.com/assets/banners/banner_dnfphoto.png",
+			URL:    "http://ohan0t6mr.bkt.clouddn.com/bar/jpg/yinyangshi.jpg",
 			Action: "com.hotkuy.app.android.activity.IntroduceActivity",
-			Params: "app_id:10003",
+			Params: "app_id:200001",
 		},
 		&models.Asset{
 			URL:    "http://oizfueie4.bkt.clouddn.com/banner_wangzherongyao.jpg",
 			Action: "com.hotkuy.app.android.activity.IntroduceActivity",
 			Params: "app_id:10004",
-		},
-		&models.Asset{
-			URL:    "http://oizfueie4.bkt.clouddn.com/assets/banners/banner_yinyangshi.png",
-			Action: "com.hotkuy.app.android.activity.IntroduceActivity",
-			Params: "app_id:200001",
 		},
 	}
 )
